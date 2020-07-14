267b56d98211b5dde4b455ac0da5c3337a164c81
BLimmie <brian01.lim@gmail.com>
2020-07-13 20:35:32 -0700

    added skills

---
+++ js/graphs/skills.js
@@ -0,0 +1,266 @@
+const skills_data = [
+    {
+        "name": "Python",
+        "value": 5,
+        "tags": [
+            "Software Languages",
+            "Data Science",
+            "Machine Learning",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Golang",
+        "value": 4.3,
+        "tags": [
+            "Software Languages",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Tensorflow",
+        "value": 2.5,
+        "tags": [
+            "Machine Learning",
+            "Data Science"
+        ]
+    },
+    {
+        "name": "C++",
+        "value": 4,
+        "tags": [
+            "Software Languages",
+            "Backend Engineering",
+            "Machine Learning"
+        ]
+    },
+    {
+        "name": "Java",
+        "value": 4,
+        "tags": [
+            "Software Languages",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Pytorch",
+        "value": 4.3,
+        "tags": [
+            "Machine Learning",
+            "Data Science"
+        ]
+    },
+    {
+        "name": "R",
+        "value": 3,
+        "tags": [
+            "Software Languages",
+            "Data Science"
+        ]
+    },
+    {
+        "name": "Javascript",
+        "value": 3,
+        "tags": [
+            "Software Languages",
+            "Data Science",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Scala",
+        "value": 2.6,
+        "tags": [
+            "Software Languages",
+            "Data Science",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Spark",
+        "value": 2.1,
+        "tags": [
+            "Software Languages",
+            "Data Science",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Ruby",
+        "value": 0.9,
+        "tags": [
+            "Software Languages"
+        ]
+    },
+    {
+        "name": "SQL",
+        "value": 3.3,
+        "tags": [
+            "Software Languages",
+            "Databases",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Graph DB",
+        "value": 3.1,
+        "tags": [
+            "Databases",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "Docker",
+        "value": 2.9,
+        "tags": [
+            "Cloud Tools",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "AWS",
+        "value": 3.9,
+        "tags": [
+            "Cloud Tools",
+            "Backend Engineering"
+        ]
+    },
+    {
+        "name": "MongoDB",
+        "value": 3,
+        "tags": [
+            "Databases",
+            "Backend Engineering"
+        ]
+    },
+]
+
+const width = document.getElementById("skills").offsetWidth
+const margin = ({top: 30, right: 40, bottom: 10, left: 10})
+const barHeight = 35
+
+function generateGraph(data) {
+    console.log(data)
+    var height = Math.ceil((data.length + 0.1) * barHeight) + margin.top + margin.bottom
+    var x = d3.scaleLinear()
+        .domain([0,d3.max(data,d => d.value)])
+        .range([margin.left, width - margin.right])
+    var y = d3.scaleBand()
+        .domain(d3.range(data.length))
+        .rangeRound([margin.top,height-margin.bottom])
+        .padding(0.1)
+    var xAxis = g => g
+        .attr("transform", `translate(0,${margin.top})`)
+        .call(d3.axisTop(x).ticks(width / 80, data.format).tickFormat(function(d){
+            switch (d) {
+                case 0.0: return "Basic";
+                case 2.5: return "Proficient";
+                case 5.0: return "Knowledgeable";
+            }
+        }))
+        .attr("font-size", 16)
+        .call(g => g.select(".domain").remove())
+    var yAxis = g => g
+        .attr("transform", `translate(${margin.left},0)`)
+    var svg = d3.select("#skills").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var bars = svg.append("g")
+        .selectAll("rect")
+        .data(data)
+        .join("rect")
+        .attr("x", x(0))
+        .attr("y", (d, i) => y(i))
+        .attr("width", 0)
+        .attr("height", y.bandwidth())
+        .attr("fill", function(d) {
+            return "rgb("+(38+(0-38)*d.value*0.2)+","+(255 + (96-255)*0.2*d.value)+"," + (155+(143-155)*0.2*d.value) + ")";
+        })
+        .transition()
+        .duration(500)
+        .attr("width", d => x(d.value) - x(0));
+    var labels = svg.append("g")
+        .attr("fill", "white")
+        .attr("text-anchor", "end")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 14)
+        .selectAll("text")
+        .data(data)
+        .join("text")
+        .attr("x", d => x(d.value))
+        .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
+        .attr("dy", "0.35em")
+        .attr("dx", -4)
+        .text(d => d.name)
+        .call(text => text.filter(d => x(d.value) - x(0) < 100) // short bars
+            .attr("dx", +4)
+            .attr("fill", "black")
+            .attr("text-anchor", "start"));
+    var gx = svg.append("g")
+        .call(xAxis);
+
+    var gy = svg.append("g")
+        .call(yAxis);
+
+    d3.selectAll("button")
+        .on("click", function() {
+            var buttonID = d3.select(this).attr("id");
+            var new_data = filterSkills(buttonID)
+            console.log(new_data)
+            bars = svg
+                .selectAll("rect")
+                .data(new_data)
+                .join("rect")
+                .attr("x", x(0))
+                .attr("y", (d, i) => y(i))
+                .attr("width", 0)
+                .attr("height", y.bandwidth())
+                .attr("fill", function(d) {
+                    return "rgb("+(38+(0-38)*d.value*0.2)+","+(255 + (96-255)*0.2*d.value)+"," + (155+(143-155)*0.2*d.value) + ")";
+                })
+                .transition()
+                .duration(500)
+                .attr("width", d => x(d.value) - x(0));
+
+            labels
+                .style("opacity",0)
+                .remove();
+            labels = svg.append("g")
+                .attr("fill", "white")
+                .attr("text-anchor", "end")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", 14)
+                .selectAll("text")
+                .data(new_data)
+                .join("text")
+                .attr("x", d => x(d.value))
+                .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
+                .attr("dy", "0.35em")
+                .attr("dx", -4)
+                .text(d => d.name)
+                .call(text => text.filter(d => x(d.value) - x(0) < 100) // short bars
+                    .attr("dx", +4)
+                    .attr("fill", "black")
+                    .attr("text-anchor", "start"));
+        })
+}
+
+function filterSkills(tag = "None") {
+    let data = [];
+    if (tag === "None") {
+        skills_data.forEach((function (s) {
+            data.push(s)
+        }));
+    } else {
+        skills_data.forEach((function (s) {
+                if (s.tags.includes(tag)) {
+                    data.push(s);
+                }
+            }
+        ))
+    }
+    return data
+}
+generateGraph(skills_data.sort((a,b) => b.value - a.value));
---
+++ skills.md
@@ -0,0 +1,18 @@
+---
+layout: page
+title: Technical Skills
+subtitle: Made in D3
+---
+<div id="header" align="center">
+    <!-- <h2>Welcome</h2> -->
+    <button type='button' id="None">All</button>
+    <button type='button' id='Software Languages'>Software Languages</button>
+    <button type='button' id="Data Science">Data Science</button>
+    <button type='button' id="Backend Engineering">Backend Engineering</button>
+    <button type='button' id="Cloud Tools">Cloud Tools</button>
+    <button type='button' id="Databases">Databases</button>
+</div>
+<div id="skills" style="width:70%; margin: 0 auto;" ></div>
+
+<script src="js/d3.js"></script>
+<script src="js/graphs/skills.js"></script>
